cb55522dadbc749f599ef5459355a4860e5c20d4
yohanbarbosa01 <ycbarbosam@ufpso.edu.co>
2023-05-12 21:15:19 -0500

    solution the error

--- js/myjs.js
+++ js/myjs.js
@@ -1,23 +1,20 @@
+alert ("bienvenido a mi pagina");    
 let enlaces_header = document.querySelectorAll(".enlaces-header")[0];
-let icon_hamburguer = true;
-
+let ico_hamburguer = true;
 
 document.querySelectorAll(".hamburguer")[0].addEventListener("click", function(){
-    if(icon_hamburguer){
-        document.querySelectorAll(".hamburguer")[0].style.color="#fff";
-        icon_hamburguer = false;
-
+    if(ico_hamburguer){
+        document.querySelectorAll(".hamburguer")[0].style.color ="#fff";
+        ico_hamburguer= false;
     }else{
-        document.querySelectorAll(".hamburguer")[0].style.color="#000";
-        icon_hamburguer = true;
+        document.querySelectorAll(".hamburguer")[0].style.color ="#000";
+        ico_hamburguer= true;
     }
 
-    enlaces-header.classList.toggle("hamburgueropen");
-
+    enlaces_header.classList.toggle("hamburgueropen");
 })
 
-document.querySelector(".enlaces-header")[0].addEventListener("click",function(){
-    enlaces-header.classList.toggle("hamburgueropen"); 
-    document.querySelectorAll(".hamburguer")[0].style.color="#000"; 
-    
+document.querySelectorAll(".enlaces-header")[0].addEventListener("click", function(){
+    enlaces_header.classList.toggle("hamburgueropen");
+    document.querySelectorAll(".hamburguer")[0].style.color ="#000";
 })
